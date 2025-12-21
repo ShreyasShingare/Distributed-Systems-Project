44071d6f1307ae10cb5f20010ba4cd1faed2bb45
ABHIJIT BASTIA <abhijitbastia1314@gmail.com>
2025-12-21 16:14:57 +0000

    Moved all services into parent services directory

--- booking-services/src/main/resources/db/migration/V4__Remove_user_slot_unique_constraint.sql
+++
@@ -1,4 +0,0 @@
--- Remove the unique constraint that prevents same user from booking same slot multiple times
--- This allows users to make multiple bookings for the same time slot
-ALTER TABLE bookings DROP CONSTRAINT IF EXISTS unique_user_amenity_slot;
-
--- services/src/main/java/com/apartmentcommunity/booking/exception/BookingValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.apartmentcommunity.booking.exception;
-
-public class BookingValidationException extends RuntimeException {
-    public BookingValidationException(String message) {
-        super(message);
-    }
-}
-
